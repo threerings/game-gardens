8de9ff7c63277271420c2f8f0d31442a1a414f01
Michael Bayne <mdb@samskivert.com>
2005-03-03 21:18:31 +0000

    Sort the games alphabetically in the listing.

--- projects/gardens/src/java/com/threerings/gardens/logic/browse.java
+++ projects/gardens/src/java/com/threerings/gardens/logic/browse.java
@@ -3,9 +3,15 @@
 
 package com.threerings.gardens.logic;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 import com.samskivert.servlet.user.User;
 import com.samskivert.servlet.util.ParameterUtil;
 import com.samskivert.velocity.InvocationContext;
+
+import com.threerings.toybox.server.persist.Game;
 
 import com.threerings.gardens.Log;
 import com.threerings.gardens.GardensApp;
@@ -22,14 +28,23 @@ public class browse extends OptionalUserLogic
         String category = ParameterUtil.getParameter(
             ctx.getRequest(), "category", false);
 
+        ArrayList<Game> games;
         if (category.equals("")) {
             // load up the metadata for all of our games
-            ctx.put("games", app.getToyBoxRepository().loadGames());
+            games = app.getToyBoxRepository().loadGames();
 
         } else {
             // load up the metadata for the games in this category
             ctx.put("category", category);
-            ctx.put("games", app.getToyBoxRepository().loadGames(category));
+            games = app.getToyBoxRepository().loadGames(category);
         }
+
+        // sort our games by name
+        Collections.sort(games, new Comparator<Game>() {
+            public int compare (Game g1, Game g2) {
+                return g1.name.compareTo(g2.name);
+            }
+        });
+        ctx.put("games", games);
     }
 }
