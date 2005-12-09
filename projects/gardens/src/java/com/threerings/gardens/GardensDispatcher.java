5da7aed4a9f44df72f71c775cd1cde7febf15455
Michael Bayne <mdb@samskivert.com>
2005-12-09 01:16:12 +0000

    Updates to remove most of our custom Velocity modifications.

--- projects/gardens/src/java/com/threerings/gardens/GardensDispatcher.java
+++ projects/gardens/src/java/com/threerings/gardens/GardensDispatcher.java
@@ -13,7 +13,6 @@ import org.apache.velocity.runtime.RuntimeSingleton;
 import com.samskivert.velocity.DispatcherServlet;
 import com.samskivert.velocity.InvocationContext;
 import com.samskivert.velocity.Logic;
-import com.samskivert.velocity.SiteResourceKey;
 
 /**
  * Handles some custom business with regard to Velocity dispatching.
@@ -24,19 +23,20 @@ public class GardensDispatcher extends DispatcherServlet
     protected Template selectTemplate (int siteId, InvocationContext ctx)
         throws ResourceNotFoundException, ParseErrorException, Exception
     {
-        // create a site resource key based on the template path and the
-        // id of the site through which this request was made
+        // do some massaging of the path to handle JNLP files in a way that
+        // doesn't confuse Java Web Start (it doesn't allow JNLP files to
+        // differ only by query parameters)
         String path = ctx.getRequest().getServletPath();
-
-        // do some massaging of the path to handle JNLP files in a way
-        // that doesn't confuse Java Web Start (it doesn't allow JNLP
-        // files to differ only by query parameters)
         if (_jnlppat.matcher(path).matches()) {
             path = "/game_jnlp.wm";
         }
 
-        return RuntimeSingleton.getRuntimeServices().getTemplate(
-            new SiteResourceKey(siteId, path));
+        if (_usingSiteLoading) {
+            // if we're using site resource loading, we need to prefix the path
+            // with the site identifier
+            path = siteId + ":" + path;
+        }
+        return RuntimeSingleton.getRuntimeServices().getTemplate(path);
     }
 
     @Override // documentation inherited
