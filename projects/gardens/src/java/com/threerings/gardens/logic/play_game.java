2b54097a93bbfd2f193d60549066eb8799b88f55
Michael Bayne <mdb@samskivert.com>
2006-08-25 02:45:56 +0000

    Added an applet launch page.

---
+++ projects/gardens/src/java/com/threerings/gardens/logic/play_game.java
@@ -0,0 +1,25 @@
+//
+// $Id$
+
+package com.threerings.gardens.logic;
+
+import com.samskivert.velocity.InvocationContext;
+import com.samskivert.servlet.user.User;
+
+import com.threerings.toybox.server.ToyBoxConfig;
+
+import com.threerings.gardens.GardensApp;
+
+/**
+ * Fires up a game in an applet.
+ */
+public class play_game extends view_game
+{
+    // documentation inherited
+    public void invoke (InvocationContext ctx, GardensApp app, User user)
+        throws Exception
+    {
+        super.invoke(ctx, app, user);
+        ctx.put("port", ToyBoxConfig.getServerPort());
+    }
+}
