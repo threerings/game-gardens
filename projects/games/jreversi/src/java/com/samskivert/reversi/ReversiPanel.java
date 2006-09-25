c1131faded6b7cb04aef020cec20b4751679d865
Michael Bayne <mdb@samskivert.com>
2006-09-25 03:14:19 +0000

    Some tidying up of the game UI.

--- projects/games/jreversi/src/java/com/samskivert/reversi/ReversiPanel.java
+++ projects/games/jreversi/src/java/com/samskivert/reversi/ReversiPanel.java
@@ -13,10 +13,12 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import com.samskivert.swing.GroupLayout;
+import com.samskivert.swing.MultiLineLabel;
 
 import com.threerings.crowd.client.PlacePanel;
 import com.threerings.util.MessageBundle;
 
+import com.threerings.toybox.client.ToyBoxUI;
 import com.threerings.toybox.client.ChatPanel;
 import com.threerings.toybox.util.ToyBoxContext;
 
@@ -43,16 +45,19 @@ public class ReversiPanel extends PlacePanel
 	setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         setLayout(new BorderLayout());
 
+        // create a container that will hold our board view in its center
+        JPanel box = GroupLayout.makeHBox();
         // create and add our board view
-        add(bview = new ReversiBoardView(ctx, ctrl), BorderLayout.CENTER);
+        box.add(bview = new ReversiBoardView(ctx, ctrl));
+        add(box, BorderLayout.CENTER);
 
         // create a side panel to hold our chat and other extra interfaces
         JPanel sidePanel = GroupLayout.makeVStretchBox(5);
 
         // add a big fat label
-        JLabel vlabel = new JLabel(msgs.get("m.title"));
-        vlabel.setFont(new Font("Helvetica", Font.BOLD, 24));
-        vlabel.setForeground(Color.black);
+        MultiLineLabel vlabel = new MultiLineLabel(msgs.get("m.title"));
+        vlabel.setAntiAliased(true);
+        vlabel.setFont(ToyBoxUI.fancyFont);
         sidePanel.add(vlabel, GroupLayout.FIXED);
 
         // a score display or other useful status indicators can go here
