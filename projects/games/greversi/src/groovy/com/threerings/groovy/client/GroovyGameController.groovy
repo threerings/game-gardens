f29868909733824c39fc99c16bb247bb81481d19
Michael Bayne <mdb@samskivert.com>
2010-12-22 01:37:14 +0800

    Compact header, BSD license. I wonder if this Groovy stuff is anywhere close to functional after all these years of bitrot?

--- projects/games/greversi/src/groovy/com/threerings/groovy/client/GroovyGameController.groovy
+++ projects/games/greversi/src/groovy/com/threerings/groovy/client/GroovyGameController.groovy
@@ -1,21 +1,6 @@
 //
-// ToyBox library - framework for matchmaking networked games
-// Copyright (C) 2005-2011 Three Rings Design, Inc., All Rights Reserved
-// http://github.com/samskivert/game-gardens
-//
-// This library is free software; you can redistribute it and/or modify it
-// under the terms of the GNU Lesser General Public License as published
-// by the Free Software Foundation; either version 2.1 of the License, or
-// (at your option) any later version.
-//
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+// Reversi - A reversi implementation for the Game Gardens platform
+// http://github.com/samskivert/game-gardens/blob/master/projects/games/greversi/LICENSE
 
 package com.threerings.groovy.client;
 
